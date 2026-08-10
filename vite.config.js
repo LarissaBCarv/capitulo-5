0b6db6a3dd944d3f2328d9a88f193929eb86f48a
LarissaBCarv <larissa.bcarvalho@live.com>
2026-08-10 01:47:24 -0300

    IOS icon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+VitePWA({
+  registerType: "autoUpdate",
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  manifest: {
+    id: "/",
+    name: "Capítulo 5",
+    short_name: "Capítulo 5",
+    description: "Uma história que continua sendo escrita.",
+
+    theme_color: "#000000",
+    background_color: "#000000",
+
+    display: "standalone",
+    orientation: "portrait",
+
+    icons: [
+      {
+        src: "/images/eyes.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/images/eyes.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+}),
