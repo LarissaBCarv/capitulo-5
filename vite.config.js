52c0a4a599c36fd2ee983eba2ae0b5930ccf1fb3
LarissaBCarv <larissa.bcarvalho@live.com>
2026-08-10 01:52:32 -0300

    IOS icon

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,39 @@
-VitePWA({
-  registerType: "autoUpdate",
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-  manifest: {
-    id: "/",
-    name: "Capítulo 5",
-    short_name: "Capítulo 5",
-    description: "Uma história que continua sendo escrita.",
+export default defineConfig({
+  plugins: [
+    react(),
 
-    theme_color: "#000000",
-    background_color: "#000000",
+    VitePWA({
+      registerType: "autoUpdate",
 
-    display: "standalone",
-    orientation: "portrait",
+      manifest: {
+        id: "/",
+        name: "Capítulo 5",
+        short_name: "Capítulo 5",
+        description: "Uma história que continua sendo escrita.",
 
-    icons: [
-      {
-        src: "/images/eyes.png",
-        sizes: "192x192",
-        type: "image/png",
+        theme_color: "#000000",
+        background_color: "#000000",
+
+        display: "standalone",
+        orientation: "portrait",
+
+        icons: [
+          {
+            src: "/images/eyes.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/eyes.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-      {
-        src: "/images/eyes.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
-}),
+    }),
+  ],
+});
